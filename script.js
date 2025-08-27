08fc1d46b7d703a97e3762919b1eed25d7d07932
shahmansoor1111 <shahmansoor0316@gmail.com>
2025-08-27 20:38:03 +0500

    gg

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-gsap.to("div" , {
-y:100,
-duration:2,
-stagger:0.2,
-yoyo: true,
-repeat:-1,
-ease : "sine.inOut"
+// gsap.to("div" , {
+// y:100,
+// duration:2,
+// stagger:0.2,
+// yoyo: true,
+// repeat:-1,
+// ease : "sine.inOut"
+// })
+gsap.to(".faraz1",{
+    x :1000,
+    duration: -2,
+    stagger: 1
 })
